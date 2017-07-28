4ba19546931f68d7bfeb34995f08cacf026f79ef
doufanther <zhengshufa@126.com>
2017-07-28 17:41:13 +0800

    01

--- src/components/shufa.vue
+++ src/components/shufa.vue
@@ -1,6 +1,11 @@
 <template>
   	<div class="shufa">
-  			8888
+  			<ul
+  v-infinite-scroll="loadMore"
+  infinite-scroll-disabled="loading"
+  infinite-scroll-distance="10">
+  <li v-for="item in list">{{ item }}</li>
+</ul>
   	</div>
 </template>
 
@@ -10,12 +15,21 @@
   export default {
     data() {
       return {
-        
+        list:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
       }
     },
     
     methods:{
-    
+    	loadMore() {
+		  this.loading = true;
+		  setTimeout(() => {
+		    let last = this.list[this.list.length - 1];
+		    for (let i = 1; i <= 10; i++) {
+		      this.list.push(last + i);
+		    }
+		    this.loading = false;
+		  }, 2500);
+		}
     }
   }
 </script>
