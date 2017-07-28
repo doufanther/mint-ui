0a57f913fa0171fc6f72998195b77c399fe2b152
doufanther <zhengshufa@126.com>
2017-07-28 16:41:22 +0800

    0

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+		<shufa></shufa>
+	</div>
+</template>
+
+<script>
+	
+	import shufa from './components/shufa.vue'
+	
+	export default {
+	  name: 'app',
+	 	components:{
+	  	'shufa':shufa
+		},
+	  methods:{
+	  
+			
+	  }
+	}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/shufa.vue
@@ -0,0 +1,21 @@
+<template>
+  	<div class="shufa">
+  			8888
+  	</div>
+</template>
+
+<script>
+	
+	
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    
+    methods:{
+    
+    }
+  }
+</script>
