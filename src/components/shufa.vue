94906d4cdd355223b28144c4cf6947db9b006c8f
doufanther <zhengshufa@126.com>
2017-07-30 12:47:17 +0800

    7-30  12：47

--- src/components/shufa.vue
+++ src/components/shufa.vue
@@ -1,24 +1,36 @@
 <template>
   	<div class="shufa">
   			<ul
-  v-infinite-scroll="loadMore"
-  infinite-scroll-disabled="loading"
-  infinite-scroll-distance="10">
-  <li v-for="item in list">{{ item }}</li>
-</ul>
+			  v-infinite-scroll="loadMore"
+			  infinite-scroll-disabled="loading"
+			  infinite-scroll-distance="10">
+			  	<li v-for="item in list">{{ item }}</li>
+			</ul>
+			<p @click="toast">toast</p>
+			<p @click="MessageBox">MessageBox</p>
+			<p @click="aa">22</p>
+			<mt-popup
+			  v-model="popupVisible"
+			  position="top"
+			>
+				<h1>兜兜</h1>
+			</mt-popup>
   	</div>
 </template>
 
 <script>
-	
+	import { Toast,MessageBox, Popup } from 'mint-ui';
 	
   export default {
     data() {
       return {
-        list:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+        list:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+        popupVisible:false
       }
     },
-    
+//  components:{
+//  	Popup
+//  },
     methods:{
     	loadMore() {
 		  this.loading = true;
@@ -29,6 +41,18 @@
 		    }
 		    this.loading = false;
 		  }, 2500);
+		},
+		toast(){
+			Toast({
+			  message: '操作成功',
+			  iconClass: 'icon icon-success'
+			});
+		},
+		MessageBox(){
+			MessageBox.prompt({message:'nihao', title:'888'});
+		},
+		aa(){
+			this.popupVisible=true;
 		}
     }
   }
