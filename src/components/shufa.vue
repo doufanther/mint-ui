9db11ccc0a96c925b7e8d45b0a6d6b9b3bdd0c70
doufanther <zhengshufa@126.com>
2017-08-01 15:16:37 +0800

    finshed

--- src/components/shufa.vue
+++ src/components/shufa.vue
@@ -33,7 +33,84 @@
 				<div slot="start"></div>
 				<div slot="end">{{val}}</div>
 			</mt-progress>
-			<mt-picker :slots="slots" @change="onValuesChange"></mt-picker>
+			<p>{{selected}}</p>
+			<!--<mt-tabbar v-model="selected">
+				  <mt-tab-item id="外卖">
+				    <img slot="icon" src="../assets/logo.png">
+				    外卖
+				  </mt-tab-item>
+				  <mt-tab-item id="订单">
+				    <img slot="icon" src="../assets/logo.png">
+				    订单
+				  </mt-tab-item>
+				  <mt-tab-item id="发现">
+				    <img slot="icon" src="../assets/logo.png">
+				    发现
+				  </mt-tab-item>
+				  <mt-tab-item id="我的">
+				    <img slot="icon" src="../assets/logo.png">
+				    我的
+				  </mt-tab-item>
+			</mt-tabbar>-->
+			<!--<mt-navbar v-model="selected">
+				  <mt-tab-item id="1">选项一</mt-tab-item>
+				  <mt-tab-item id="2">选项二</mt-tab-item>
+				  <mt-tab-item id="3">选项三</mt-tab-item>
+			</mt-navbar>
+			
+			tab-container 
+			<mt-tab-container v-model="selected">
+				  <mt-tab-container-item id="1">
+				    <mt-cell v-for="n in 10" :title="'内容 ' + n" />
+				  </mt-tab-container-item>
+				  <mt-tab-container-item id="2">
+				    <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+				  </mt-tab-container-item>
+				  <mt-tab-container-item id="3">
+				    <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+				  </mt-tab-container-item>
+			</mt-tab-container>-->
+			<mt-button size="small">small</mt-button>
+			<mt-button size="large">large</mt-button>
+			<mt-button size="normal">normal</mt-button>
+			<mt-button plain>plain</mt-button>
+			<mt-button icon="back">back</mt-button>
+			<mt-button icon="more">更多</mt-button>
+			<div ref="dec">
+				<mt-cell-swipe
+				  title="标题文字"
+				  :right="[
+				    {
+				      content: '删除',
+				      style: { background: 'red', color: '#fff' },
+				      handler: () =>{this.$messagebox.confirm('确定执行此操作?').then(action => {
+	 									bb();		
+									});
+								}
+				    }
+				  ]" >
+				  </mt-cell-swipe>
+			</div>
+			<!--<mt-search
+			  v-model="value"
+			  cancel-text="取消"
+			  placeholder="搜索">
+			</mt-search>-->
+			<mt-switch v-model="value8">开关</mt-switch>
+			<mt-checklist
+			  title="复选框列表"
+			  v-model="value"
+			  :options="options">
+			</mt-checklist>
+			<div>{{value}}</div>
+			<mt-field label="生日" placeholder="请输入生日" type="date" v-model="birthday"></mt-field>
+			<div>{{birthday}}</div>
+			<div>
+				<mt-badge type="primary">30</mt-badge>
+				<mt-badge type="error">10</mt-badge>
+				<mt-badge type="success">10</mt-badge>
+				<mt-badge type="warning">10</mt-badge>
+			</div>
   	</div>
 </template>
 
@@ -47,26 +124,35 @@
         popupVisible:false,
         rangeValue:30,
         val:0,
-        slots: [
-        {
-          flex: 1,
-          values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
-          className: 'slot1',
-          textAlign: 'right'
-        }, {
-          divider: true,
-          content: '-',
-          className: 'slot2'
-        }, {
-          flex: 1,
-          values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
-          className: 'slot3',
-          textAlign: 'left'
-        }
-      ]
+      	selected:'',
+      	value8:false,
+      	value:[],
+      	options:'',
+      	birthday:''
       }
     },
-   
+   	created(){
+   		this.options = [
+		  {
+		    label: '被禁用',
+		    value: '值F',
+		    disabled: true
+		  },
+		  {
+		    label: '选中禁用',
+		    value: '选中禁用的值',
+		    disabled: true
+		  },
+		  {
+		    label: '选项A',
+		    value: '值A'
+		  },
+		  {
+		    label: '选项B',
+		    value: '值B'
+		  }
+		];
+   	},
     methods:{
     	loadMore() {
 		  this.loading = true;
@@ -93,6 +179,9 @@
 		handleChange(index){
 			//console.log(index);
 		},
+		bb(){
+			this.$refs.dec.remove();
+		},
 		uploading(){
 			let aa=setInterval(()=>{
 				this.val++;
@@ -104,14 +193,9 @@
 					  iconClass: 'icon icon-success'
 					});
 				}
-			},30)
+			},15)
 		},
-		onValuesChange(picker, values) {
-	      if (values[0] > values[1]) {
-	        picker.setSlotValue(1, values[0]);
-	        console.log(picker)
-	      }
-	    }
+		
     }
   }
 </script>
